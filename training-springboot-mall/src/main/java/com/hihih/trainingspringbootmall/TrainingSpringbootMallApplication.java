afcebeb6f5453695e04e7b1f4e1c99b8890aa828
hihihmm <freedom80265@gmail.com>
2022-06-30 13:03:45 +0800

    初始化 Spring Boot project

---
+++ training-springboot-mall/src/main/java/com/hihih/trainingspringbootmall/TrainingSpringbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.hihih.trainingspringbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingSpringbootMallApplication.class, args);
+    }
+
+}
---
+++ training-springboot-mall/src/test/java/com/hihih/trainingspringbootmall/TrainingSpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hihih.trainingspringbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
